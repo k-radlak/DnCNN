7b8ce79c2d58db1b912d1641367b45b114759e1f
Krystian Radlak <kradlak@exida.com>
2018-11-25 23:18:39 +0100

    improced patch generation for different size of images

--- TrainingCodes/dncnn_keras/data_generator.py
+++ TrainingCodes/dncnn_keras/data_generator.py
@@ -61,15 +61,15 @@ def data_aug(img, mode=0):
     elif mode == 7:
         return np.flipud(np.rot90(img, k=3))
 
-def gen_patches(file_name):
+def gen_patches(file_name,patches):
 
     # read image
     img = cv2.imread(file_name, 0)  # gray scale
     h, w = img.shape
-    patches = []
+    #patches = []
     for s in scales:
         h_scaled, w_scaled = int(h*s),int(w*s)
-        img_scaled = cv2.resize(img, (h_scaled,w_scaled), interpolation=cv2.INTER_CUBIC)
+        img_scaled = cv2.resize(img, (w_scaled,h_scaled), interpolation=cv2.INTER_CUBIC)
         # extract patches
         for i in range(0, h_scaled-patch_size+1, stride):
             for j in range(0, w_scaled-patch_size+1, stride):
@@ -79,7 +79,6 @@ def gen_patches(file_name):
                 for k in range(0, aug_times):
                     x_aug = data_aug(x, mode=np.random.randint(0,8))
                     patches.append(x_aug)
-                
     return patches
 
 def datagenerator(data_dir='data/Train400',verbose=False):
@@ -89,12 +88,12 @@ def datagenerator(data_dir='data/Train400',verbose=False):
     data = []
     # generate patches
     for i in range(len(file_list)):
-        patch = gen_patches(file_list[i])
-        data.append(patch)
+        data = gen_patches(file_list[i],data)
+        #data.append(patch)
         if verbose:
             print(str(i+1)+'/'+ str(len(file_list)) + ' is done ^_^')
     data = np.array(data, dtype='uint8')
-    data = data.reshape((data.shape[0]*data.shape[1],data.shape[2],data.shape[3],1))
+    data = data.reshape((data.shape[0],data.shape[1],data.shape[2],1))
     discard_n = len(data)-len(data)//batch_size*batch_size;
     data = np.delete(data,range(discard_n),axis = 0)
     print('^_^-training data finished-^_^')
